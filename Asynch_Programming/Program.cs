d8f689860aaa5e1e07d99f87c1784d1aff60261a
liakhivan <liakhivan21@gmail.com>
2020-09-19 09:56:34 +0300

    Добавьте файлы проекта.

---
+++ Asynch_Programming/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynch_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Id потоку методу main - {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("для запуску нажміть будь яку клавішу:");
+            Console.ReadKey();
+
+            Report();
+            ThreadPool.QueueUserWorkItem(new WaitCallback(Example1));
+            Report();
+            ThreadPool.QueueUserWorkItem(new WaitCallback(Example2));
+            Report();
+
+            Console.ReadKey();
+            Report();
+
+
+        }
+
+        private static void Example1(object state)
+        {
+            Console.WriteLine($"Метод Example1 почав виконуватися в потоці {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(2000);
+            Console.WriteLine($"Метод Example1 закінчив виконуватися в потоці {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private static void Example2(object state)
+        {
+            Console.WriteLine($"Метод Example2 почав виконуватися в потоці {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Метод Example2 закінчив виконуватися в потоці {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private static void Report()
+        {
+            ThreadPool.GetMaxThreads(out int maxWorkerThreads, out int maxPortThreads);
+            ThreadPool.GetAvailableThreads(out int workerThreads, out int portThreads);
+
+
+            Console.WriteLine($"Робочі потоки {workerThreads} з {maxWorkerThreads}");
+            Console.WriteLine($"ІО потоки {portThreads} з {maxPortThreads}");
+            Console.WriteLine(new string('-', 80));
+        }
+    }
+}
---
+++ Thread_Lesson_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(WriteChar));
+
+            Console.WriteLine("For start press any key.");
+            Console.ReadKey();
+
+            thread.Start('*');
+
+
+            for (int i = 0; i < 80; i++)
+            {
+                Console.Write('-');
+                Thread.Sleep(70);
+            }
+        }
+
+
+        private static void WriteChar(object arg)
+        {
+            char ch = (char)arg;
+            for (int i = 0; i < 80; i++)
+            {
+                Console.Write(ch);
+                Thread.Sleep(70);
+            }
+        }
+    }
+}
